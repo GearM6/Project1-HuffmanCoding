bff57c50b1d77e3ad46a2d4df33b6ebc30188930
Matt <thatmarrakid@gmail.com>
2020-02-28 11:29:37 -0800

    It kinda works, need to fix the tree implementation though

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -1,20 +1,72 @@
 import com.google.common.collect.*;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Huffman {
+
     public static SortedMultiset<Character> getValues(File file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         SortedMultiset<Character> multiset = TreeMultiset.create();
-        int character;
+        int character = 0;
         while((character = reader.read()) != -1){
                 multiset.add((char) character);
         }
         return multiset;
     }
+    private static HuffmanNode populateTree(PriorityQueue<HuffmanNode> queue) {
+        while(queue.size() > 1){
+            HuffmanNode lowerNode = queue.poll();
+            HuffmanNode higherNode = queue.poll();
+            queue.add(new HuffmanNode(lowerNode, higherNode));
+        }
+        return queue.peek();
+    }
+
+    private static void populatePriorityQueue(SortedMultiset characters, PriorityQueue<HuffmanNode> queue) {
+        while(characters.size() > 0){
+            Multiset.Entry entry = characters.pollFirstEntry();
+            queue.add(new HuffmanNode(entry.toString().charAt(0), entry.getCount()));
+        }
+    }
+
+    public static void getEncoding(HuffmanNode root, String s, Map<Character, String> map){
+        if(root.isLeaf()){
+            map.put(root.getCharacter(), s);
+            System.out.println(root.getCharacter() + ":" + s);
+            return;
+        }
+        if(root.getRight() != null){
+            getEncoding(root.getLeft(), s+"0", map);
+        }
+        if(root.getLeft() != null){
+            getEncoding(root.getRight(), s+"1", map);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        SortedMultiset characters = getValues(new File("main/resources/USConstitution.txt"));
-        System.out.println(Arrays.toString(characters.toArray()));
+        File file = new File("./USConstitution.txt");
+        SortedMultiset characters = getValues(file);
+        characters = characters.descendingMultiset(); //Reverse the set
+        PriorityQueue<HuffmanNode> queue = new PriorityQueue<>();
+        Map<Character, String> characterMap = new HashMap<>();
+
+        //Add nodes to priority queue
+        populatePriorityQueue(characters, queue);
+        HuffmanNode root = populateTree(queue);
+        getEncoding(root, "", characterMap);
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        int character;
+        StringBuilder encodedText = new StringBuilder();
+
+        while((character = reader.read()) != -1){
+            encodedText.append(characterMap.get((char) character));
+        }
+        System.out.println(encodedText.toString());
+        System.out.println(encodedText.length() + "Bytes");
+        File compressedFile = new File("./USCompressditution.txt");
     }
 }
--- src/main/java/HuffmanNode.java
+++ src/main/java/HuffmanNode.java
@@ -1,8 +1,47 @@
-public class HuffmanNode {
-    private int value;
+public class HuffmanNode implements Comparable<HuffmanNode>{
+    private int count;
+
     private char character;
+
     private HuffmanNode left;
+
     private HuffmanNode right;
+    public HuffmanNode(char character, int value){
+        this.character = character;
+        this.count = value;
+    }
 
+    public HuffmanNode getLeft() {
+        return left;
+    }
 
+    public HuffmanNode getRight() {
+        return right;
+    }
+
+    public boolean isLeaf(){
+        return (this.left == null && this.right == null);
+    }
+
+    public HuffmanNode(HuffmanNode lowerNode, HuffmanNode higherNode){
+        this.left = lowerNode;
+        this.right = higherNode;
+        this.count = lowerNode.count + higherNode.count;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    @Override
+    public int compareTo(HuffmanNode node) {
+        if(this.count >= node.count){
+            return 1;
+        }
+        return -1;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
